74db9daf0ad20d0f292cef6391803f11d8441b37
on1x22 <on1x22@mail.ru>
2023-04-08 14:45:26 +0400

    Create interface IZookeeperCatalog

--- ZookeeperCatalog/IZookeeperCatalog.cs
+++ ZookeeperCatalog/IZookeeperCatalog.cs
@@ -1,7 +1,14 @@
-﻿namespace MicroZoo.ZookeeperCatalog
+﻿using MicroZoo.ZookeeperCatalog.Models;
+
+namespace MicroZoo.ZookeeperCatalog
 {
     public interface IZookeeperCatalog
     {
-        
+        IEnumerable<Zookepeer> Get();
+
+        Zookepeer Get(string name);
+        Zookepeer Get(int id);
+
+        IEnumerable<Zookepeer> GetZookeepers(string speciality);
     }
 }
---
+++ ZookeeperCatalog/Models/Zookepeer.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroZoo.ZookeeperCatalog.Models
+{
+    public class Zookepeer
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public List<string> Speciality { get; set; }
+    }
+}
