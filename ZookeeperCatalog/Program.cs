bca70efcf8e6f4e03331380671197e437d5c284f
on1x22 <on1x22@mail.ru>
2023-04-23 22:35:11 +0400

    Removed commented text

--- ZookeeperCatalog/Program.cs
+++ ZookeeperCatalog/Program.cs
@@ -9,60 +9,9 @@ using MicroZoo.ZookeeperCatalog.Apis;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddLogging(builder => builder.AddConsole());
-//builder.Services.AddControllers();
-
-//string connectionString = builder.Configuration.GetConnectionString("ZookeeperDB");
-//builder.Services.AddDbContext<ZookeeperDBContext>(c => c.UseNpgsql(connectionString));
-//builder.Services.AddTransient<IZookeeperRepository, ZookeeperRepository>();
-
 RegisterServices(builder.Services);
 
 var app = builder.Build();
-
-
-
-/*//app.MapGet("/zookeeper/{name:string}", (IZookeeperCatalog zc, string name) =>
-app.MapGet("/zookeeper/name/{name}", (IZookeeperRepository zc, string name) =>
-{
-    var zookeeper = zc.GetByName(name);
-    if (zookeeper == null)
-        return Results.NotFound();
-
-    return Results.Ok(zookeeper);
-});
-
-//app.MapGet("/zookeeper/{id:int}", (IZookeeperCatalog zc, int id) =>
-app.MapGet("/zookeeper/id/{id}", (IZookeeperRepository zc, int id) =>
-{
-    var zookeeper = zc.GetById(id);
-    if (zookeeper == null)
-        return Results.NotFound(); 
-    
-    return Results.Ok(zookeeper);       
-});
-
-app.MapGet("/zookeeper", (IZookeeperRepository zc) =>
-{
-    var allZookeepers = zc.GetAll();
-    if (allZookeepers == null)
-        return Results.NotFound();
-
-    return Results.Ok(allZookeepers);
-});
-
-//app.MapGet("/zookeeperspeciality/{speciality:string}", (IZookeeperCatalog zc, string speciality) =>
-app.MapGet("/zookeeper/speciality/{speciality}", (IZookeeperRepository zc, string speciality) =>
-{
-    var zookepers = zc.GetZookeepersSpeciality(speciality);
-    if (zookepers == null)
-        return Results.NotFound();
-
-    return Results.Ok(zookepers);
-});
-*/
-
-//app.MapControllers();
 
 Configure(app);
 
@@ -92,5 +41,5 @@ void RegisterServices(IServiceCollection services)
 
 void Configure(WebApplication app)
 {
-    //app.UseHttpsRedirection();
+    
 }
