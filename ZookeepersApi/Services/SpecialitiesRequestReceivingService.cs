cfcfe6e11403ffdd1692a0a25ab86837ba686309
on1x22 <on1x22@mail.ru>
2024-03-22 22:58:36 +0400

    Added unit tests for AddSpeciality method in SpecialitiesRequestReceivingServiceTests

--- ZookeepersApi/Services/SpecialitiesRequestReceivingService.cs
+++ ZookeepersApi/Services/SpecialitiesRequestReceivingService.cs
@@ -1,8 +1,12 @@
-﻿using MicroZoo.Infrastructure.MassTransit;
+﻿using MassTransit;
+using MicroZoo.Infrastructure.MassTransit;
 using MicroZoo.Infrastructure.MassTransit.Requests.AnimalsApi;
+using MicroZoo.Infrastructure.MassTransit.Requests.PersonsApi;
 using MicroZoo.Infrastructure.MassTransit.Requests.ZookeepersApi;
 using MicroZoo.Infrastructure.MassTransit.Responses.AnimalsApi;
+using MicroZoo.Infrastructure.MassTransit.Responses.PersonsApi;
 using MicroZoo.Infrastructure.MassTransit.Responses.ZokeepersApi;
+using MicroZoo.Infrastructure.Models.Specialities.Dto;
 
 namespace MicroZoo.ZookeepersApi.Services
 {
@@ -35,6 +39,30 @@ namespace MicroZoo.ZookeepersApi.Services
         public async Task<CheckZokeepersWithSpecialityAreExistResponse> CheckZookeeperIsExist(CheckType checkType, int personId)
         {
             return await _specialitiesService.CheckZokeepersWithSpecialityAreExistAsync(checkType, personId);
+        }
+
+        public async Task<GetSpecialityResponse> AddSpeciality(SpecialityDto specialityDto)
+        {
+            var personResponse = await _receiver.GetResponseFromRabbitTask<GetPersonRequest, GetPersonResponse>(
+                new GetPersonRequest(specialityDto.ZookeeperId), _connectionService.PersonsApiUrl);
+
+            var animalTypeResponse = await _receiver.GetResponseFromRabbitTask<GetAnimalTypeRequest,
+                GetAnimalTypeResponse>(new GetAnimalTypeRequest(specialityDto.AnimalTypeId), _connectionService.AnimalsApiUrl);
+
+            string errorMessage = string.Empty;
+
+            if (personResponse.Person == null)
+                errorMessage += personResponse.ErrorMessage + ".\n";
+
+            if (animalTypeResponse.AnimalType == null)
+                errorMessage += animalTypeResponse.ErrorMessage;
+
+            if (errorMessage != string.Empty)            
+                return new GetSpecialityResponse() { ErrorMessage = errorMessage };
+            
+
+            return await _receiver.GetResponseFromRabbitTask<AddSpecialityRequest, GetSpecialityResponse>
+                (new AddSpecialityRequest(specialityDto), _connectionService.ZookeepersApiUrl);
         }
     }
 }
