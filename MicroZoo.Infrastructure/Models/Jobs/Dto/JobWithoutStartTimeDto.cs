2a6d55e7342693cc6f6b9783cc93c609f8b2c822
on1x22 <on1x22@mail.ru>
2025-02-01 20:18:33 +0400

    Documented classes in Models folder in MicroZoo.Infrastructure

--- MicroZoo.Infrastructure/Models/Jobs/Dto/JobWithoutStartTimeDto.cs
+++ MicroZoo.Infrastructure/Models/Jobs/Dto/JobWithoutStartTimeDto.cs
@@ -1,11 +1,30 @@
 ﻿
 namespace MicroZoo.Infrastructure.Models.Jobs.Dto
 {
+    /// <summary>
+    /// DTO obtained from controllers and other microservices and 
+    /// provides information about a job but without start time
+    /// </summary>
     public class JobWithoutStartTimeDto
     {
+        /// <summary>
+        /// Id of zookeeper that doing this job
+        /// </summary>
         public int ZookeeperId { get; set; }
-        public string Description { get; set; }
+
+        /// <summary>
+        /// Job description
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Time that job must be done
+        /// </summary>
         public DateTime DeadlineTime { get; set; }
+
+        /// <summary>
+        /// Job priority
+        /// </summary>
         public int Priority { get; set; }
     }
 }
--- MicroZoo.Infrastructure/Models/Persons/Dto/PersonDto.cs
+++ MicroZoo.Infrastructure/Models/Persons/Dto/PersonDto.cs
@@ -1,27 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MicroZoo.Infrastructure.Models.Persons.Dto
+﻿namespace MicroZoo.Infrastructure.Models.Persons.Dto
 {
+    /// <summary>
+    /// DTO obtained from controllers and other microservices and 
+    /// provides information about a person
+    /// </summary>
     public class PersonDto
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        /// <summary>
+        /// First name of person
+        /// </summary>
+        public string? FirstName { get; set; }
+
+        /// <summary>
+        /// Last name of person
+        /// </summary>
+        public string? LastName { get; set; }
+        
+        /// <summary>
+        /// Email address
+        /// </summary>
+        public string? Email { get; set; }
+        
+        /// <summary>
+        /// Is person manager or ordinary worker
+        /// </summary>
         public bool IsManager { get; set; }
+
+        /// <summary>
+        /// Id of the manager to whom the worker reports
+        /// </summary>
         public int ManagerId { get; set; }
 
+        /// <summary>
+        /// Convert instane of <see cref="PersonDto"/> class to the instance 
+        /// of the <see cref="Person"/> class
+        /// </summary>
+        /// <returns></returns>
         public Person ToPerson()
         {
             return new Person
             {
-                FirstName = FirstName,
-                LastName = LastName,
-                Email = Email,
+                FirstName = FirstName!,
+                LastName = LastName!,
+                Email = Email!,
                 IsManager = IsManager,
                 ManagerId = ManagerId
             };
