109b46e2cf2150dcaba4fa13aa23f58cef406c5a
on1x22 <on1x22@mail.ru>
2025-02-02 16:22:39 +0400

    Documented classes in MassTransit folder in MicroZoo.InfrastructureMassTransit.Requests.IdentityApi, MassTransit.Responses.PersonsApi Models folder in MicroZoo.Infrastructure

--- MicroZoo.Infrastructure/MassTransit/ErrorCodes.cs
+++ MicroZoo.Infrastructure/MassTransit/ErrorCodes.cs
@@ -1,12 +1,38 @@
 ﻿namespace MicroZoo.Infrastructure.MassTransit
 {
+    /// <summary>
+    /// List of status codes of responces
+    /// </summary>
     public enum ErrorCodes
     {
+        /// <summary>
+        /// Everithing is good
+        /// </summary>
         Ok200,
+
+        /// <summary>
+        /// Request has invalid data
+        /// </summary>
         BadRequest400,
+
+        /// <summary>
+        /// User is not authorized
+        /// </summary>
         Unauthorized401,
+
+        /// <summary>
+        /// The user does not have the necessary permissions
+        /// </summary>
         Forbiden403,
+
+        /// <summary>
+        /// Requested resource does not exist
+        /// </summary>
         NotFound404,
+
+        /// <summary>
+        /// An unexpected error occurred while processing request
+        /// </summary>
         InternalServerError500
     }
 }
