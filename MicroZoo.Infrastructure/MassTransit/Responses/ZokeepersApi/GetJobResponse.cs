e4991ed3ad337e88692834381c010a52f44d7ccd
on1x22 <on1x22@mail.ru>
2025-02-15 21:53:19 +0400

    Documented GetJobResponse class in MassTransit.Responses.ZookeepersApi in MicroZoo.Infrastructure

--- MicroZoo.Infrastructure/MassTransit/Responses/ZokeepersApi/GetJobResponse.cs
+++ MicroZoo.Infrastructure/MassTransit/Responses/ZokeepersApi/GetJobResponse.cs
@@ -2,11 +2,29 @@
 
 namespace MicroZoo.Infrastructure.MassTransit.Responses.ZokeepersApi
 {
+    /// <summary>
+    /// Provides information about job for response through RabbitMq
+    /// </summary>
     public class GetJobResponse : IResponseWithError
     {
+        /// <summary>
+        /// Id of the request
+        /// </summary>
         public Guid OperationId { get; set; }
-        public Job Job { get; set; }
+
+        /// <summary>
+        /// Information about job
+        /// </summary>
+        public Job? Job { get; set; }
+
+        /// <summary>
+        /// Message describing error
+        /// </summary>
         public string? ErrorMessage { get; set; }
+
+        /// <summary>
+        /// Http code of occurred error
+        /// </summary>
         public ErrorCodes? ErrorCode { get; set; }
     }
 }
