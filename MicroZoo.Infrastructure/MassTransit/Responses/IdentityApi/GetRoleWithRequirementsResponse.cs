cc48326814d81b4a1fe448eadb1d8c2f99cfbe8e
on1x22 <on1x22@mail.ru>
2025-03-18 22:22:45 +0400

    Documented GetRoleWithRequirementResponse class in MassTransit.Responses.IdentityApi in MicroZoo.Infrastructure

--- MicroZoo.Infrastructure/MassTransit/Responses/IdentityApi/GetRoleWithRequirementsResponse.cs
+++ MicroZoo.Infrastructure/MassTransit/Responses/IdentityApi/GetRoleWithRequirementsResponse.cs
@@ -2,10 +2,24 @@
 
 namespace MicroZoo.Infrastructure.MassTransit.Responses.IdentityApi
 {
+    /// <summary>
+    /// Provides information about role with requirements for response through RabbitMq
+    /// </summary>
     public class GetRoleWithRequirementsResponse
     {
+        /// <summary>
+        /// Id of the request
+        /// </summary>
         public Guid OperationId { get; set; }
+
+        /// <summary>
+        /// Information about role with requirements
+        /// </summary>
         public RoleWithRequirements? RoleWithRequirements { get; set; }
+
+        /// <summary>
+        /// Message describing error
+        /// </summary>
         public string? ErrorMessage { get; set; }
     }
 }
