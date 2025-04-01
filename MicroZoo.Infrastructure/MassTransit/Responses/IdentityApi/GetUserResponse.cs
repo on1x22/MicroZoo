d3a2d20661a34893e4c9f6547ab8ca521bd5ce82
on1x22 <on1x22@mail.ru>
2025-04-01 20:49:17 +0400

    Documented GetUserResponse class in MassTransit.Responses.IdentityApi in MicroZoo.Infrastructure

--- MicroZoo.Infrastructure/MassTransit/Responses/IdentityApi/GetUserResponse.cs
+++ MicroZoo.Infrastructure/MassTransit/Responses/IdentityApi/GetUserResponse.cs
@@ -1,16 +1,25 @@
 ﻿using MicroZoo.Infrastructure.Models.Users;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MicroZoo.Infrastructure.MassTransit.Responses.IdentityApi
 {
+    /// <summary>
+    /// Provides information about user for response through RabbitMq
+    /// </summary>
     public record GetUserResponse
     {
+        /// <summary>
+        /// Id of the request
+        /// </summary>
         public Guid OperationId { get; set; }
+
+        /// <summary>
+        /// Information about user
+        /// </summary>
         public User? User { get; set; }
+
+        /// <summary>
+        /// Message describing error
+        /// </summary>
         public string? ErrorMessage { get; set; }
     }
 }
