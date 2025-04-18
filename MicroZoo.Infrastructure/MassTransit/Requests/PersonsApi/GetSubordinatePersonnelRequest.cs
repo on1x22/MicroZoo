702e76eb29b535cbec099ebb7ea76c3298605e1a
on1x22 <on1x22@mail.ru>
2025-04-18 22:22:06 +0400

    Documented classes in MassTransit.Requests.PersonsApi in MicroZoo.Infrastructure

--- MicroZoo.Infrastructure/MassTransit/Requests/PersonsApi/GetSubordinatePersonnelRequest.cs
+++ MicroZoo.Infrastructure/MassTransit/Requests/PersonsApi/GetSubordinatePersonnelRequest.cs
@@ -1,11 +1,31 @@
 ﻿namespace MicroZoo.Infrastructure.MassTransit.Requests.PersonsApi
 {
+    /// <summary>
+    /// Allows to get information about subordinate personnel of person 
+    /// by request receiving from RabbitMq
+    /// </summary>
     public class GetSubordinatePersonnelRequest
     {
+        /// <summary>
+        /// Id of the request
+        /// </summary>
         public Guid OperationId { get; set; }
+
+        /// <summary>
+        /// Id of person that managing of personnel
+        /// </summary>
         public int PersonId { get; set; }
+
+        /// <summary>
+        /// AccessToken for check authentication
+        /// </summary>
         public string AccessToken { get; }
 
+        /// <summary>
+        /// Initialize a new instance of <see cref="GetSubordinatePersonnelRequest"/> class
+        /// </summary>
+        /// <param name="personId"></param>
+        /// <param name="accessToken"></param>
         public GetSubordinatePersonnelRequest(int personId, string accessToken)
         {
             OperationId = Guid.NewGuid();
