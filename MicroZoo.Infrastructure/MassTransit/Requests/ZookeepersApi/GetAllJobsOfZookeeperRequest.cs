1a06a5d7a459bc0ecf909585a88822f2cda47a8b
on1x22 <on1x22@mail.ru>
2025-04-19 22:06:21 +0400

    Documented classes in MassTransit.Requests.ZookeepersApi in MicroZoo.Infrastructure

--- MicroZoo.Infrastructure/MassTransit/Requests/ZookeepersApi/GetAllJobsOfZookeeperRequest.cs
+++ MicroZoo.Infrastructure/MassTransit/Requests/ZookeepersApi/GetAllJobsOfZookeeperRequest.cs
@@ -1,11 +1,31 @@
 ﻿namespace MicroZoo.Infrastructure.MassTransit.Requests.ZookeepersApi
 {
+    /// <summary>
+    /// Allows to get list all jobs by request receiving from RabbitMq. This list 
+    /// consist of done and undone jobs
+    /// </summary>
     public class GetAllJobsOfZookeeperRequest
     {
+        /// <summary>
+        /// Id of the request
+        /// </summary>
         public Guid OperationId { get; set; }
+
+        /// <summary>
+        /// Zookeeper's Id
+        /// </summary>
         public int ZookeeperId { get; set; }
+
+        /// <summary>
+        /// AccessToken for check authentication
+        /// </summary>
         public string AccessToken { get; }
 
+        /// <summary>
+        /// Initialize a new instance of <see cref="GetAllJobsOfZookeeperRequest"/> class
+        /// </summary>
+        /// <param name="zookeeperId"></param>
+        /// <param name="accessToken"></param>
         public GetAllJobsOfZookeeperRequest(int zookeeperId, string accessToken)
         {
             OperationId = Guid.NewGuid();
