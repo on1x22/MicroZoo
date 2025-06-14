1555a30c62c63b46dbd0e4129ecf583a4d665339
on1x22 <on1x22@mail.ru>
2025-06-14 14:02:58 +0400

    Added UserRequirementsServiceTests for IdentityApi.Tests

---
+++ Tests/IdentityApi.Tests/UnitTests/Services/UserRequirementsServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using MicroZoo.IdentityApi.Repositories;
+using MicroZoo.IdentityApi.Services;
+using MicroZoo.Infrastructure.Models.Users;
+using Moq;
+
+namespace MicroZoo.IdentityApi.Tests.UnitTests.Services
+{
+    public class UserRequirementsServiceTests
+    {
+        private readonly IFixture _fixture;
+        private readonly Mock<IUserRequirementsRepository> _userRequirementsRepository;
+        private readonly UserRequirementsService _userRequirementsService;
+
+        public UserRequirementsServiceTests()
+        {
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+            _userRequirementsRepository = _fixture.Freeze<Mock<IUserRequirementsRepository>>();
+
+            _userRequirementsService = _fixture.Create<UserRequirementsService>();
+        }
+
+        [Fact]
+        public async Task GetAllowedRequirementsOfUser_CorrectData_ReturnsAllowedRequirementsOfUser()
+        {
+            // Arrange
+            var user = _fixture.Build<User>().Create();
+            var requirements = _fixture.Build<string>()
+                .CreateMany(5)
+                .ToList();
+
+            _userRequirementsRepository.Setup(x => x.GetAllowedRequirementsOfUser(It.IsAny<User>()))
+                .ReturnsAsync(requirements);
+
+            // Act
+            var result = await _userRequirementsService.GetAllowedRequirementsOfUser(user);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Equal(5, result.Count);
+        }
+    }
+}
